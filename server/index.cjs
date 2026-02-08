27d1ad168b65a37e6e689a38f5022f4e5581ff32
MS Solutions <info@msforsolutions.com>
2026-02-08 11:15:29 +0300

    Fix: Restore missing path module import

--- server/index.cjs
+++ server/index.cjs
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const db = require('./database.cjs');
+const path = require('path');
 
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
